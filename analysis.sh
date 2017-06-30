097354364dd12ccf2f7e9206a2adfd32ad3ec0af
Hamed Saleh <hameelas@gmail.com>
2017-06-30 21:35:14 +0430

    Remove reduntant calculations

--- analysis.sh
+++ analysis.sh
@@ -4,20 +4,7 @@ tps_url="https://tps.ioi2017.org/gitted/problem"
 
 commit=$(git log --pretty=format:'%H' -n 1)
 
-repo_url=$(git config --local remote.origin.url)
-
-if [ -z ${repo_url+x} ]; then
-    errcho "Cannot find the 'origin' remote url to extract repo name"
-    exit 1
-fi
-
-repo_name=$(basename -s .git ${repo_url})
-
-analysis_url=${tps_url}/${repo_name}/${commit}/analysis
-
-# problem_name=$(json_extract.sh ${base_dir}/problem.json name)
-#
-# analysis_url=${tps_url}/${problem_name}/${commit}/analysis
+analysis_url=${tps_url}/${problem_name}/${commit}/analysis
 
 echo ${analysis_url}
 
