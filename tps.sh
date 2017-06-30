cf4ffca8928f33f99437aa4890ff398c2f157c17
Kian Mirjalali <mirjalali@gmail.com>
2017-06-30 23:06:23 +0430

    hid variables using function and renaming

--- tps.sh
+++ tps.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # A supplimentary command as a tool used in TPS repositories
-# Kian Mirjalali
+# Kian Mirjalali, Hamed Saleh
 # IOI 2017, Iran
 
 
-version=1.0
+tps_version=1.0
 
 
 
@@ -15,74 +15,67 @@ function errcho {
 	>&2 echo "$@"
 }
 
-function usage {
-	errcho "TPS version ${version}"
+function tps_usage {
+	errcho "TPS version ${tps_version}"
 	errcho "Usage: tps <command> <arguments>..."
 	exit 1
 }
 
 
-[ $# -gt 0 ] || usage
+[ $# -gt 0 ] || tps_usage
 command=$1; shift
 
-
-target_file=problem.json
-
-#looking for ${target_file} in current and parent directories...
-curr=$PWD
-while [ "${curr}" != "${old}" ] ; do
-	if [ -f "${curr}/${target_file}" ] ; then
-		base_dir="${curr}"
-		break
+function find_base_dir {	
+	target_file=problem.json
+	
+	#looking for ${target_file} in current and parent directories...
+	curr=$PWD
+	while [ "${curr}" != "${old}" ] ; do
+		if [ -f "${curr}/${target_file}" ] ; then
+			base_dir="${curr}"
+			break
+		fi
+		old="${curr}"
+		curr=$(dirname ${curr})
+	done
+	
+	if [ -z ${base_dir+x} ]; then
+		errcho "Error: Not a TPS repository (${target_file} not found in any of the parent directories)"
+		exit 2
 	fi
-	old="${curr}"
-	curr=$(dirname ${curr})
-done
+	echo ${base_dir}
+}
 
-
-if [ -z ${base_dir+x} ]; then
-	errcho "Fatal: Not a TPS repository (${target_file} not found in any of the parent directories)"
-	exit 2
-fi
-
+base_dir=$(find_base_dir)
 export base_dir
 
 
+__scripts__="scripts"
+__scripts_dir__="${base_dir}/${__scripts__}"
 
-scripts="scripts"
-internals="${scripts}/internal"
-tps_init=${internals}/tps_init.sh
-
-scripts_dir="${base_dir}/${scripts}"
-internals_dir="${base_dir}/${internals}"
-tps_init_file="${base_dir}/${tps_init}"
-
-
-if [ ! -d "${scripts_dir}" ] ; then
-	errcho "Error: Directory '${scripts}.' not found"
+if [ ! -d "${__scripts_dir__}" ] ; then
+	errcho "Error: Directory '${__scripts__}.' not found"
 	exit 2
 fi
 
-if [ ! -d "${internals_dir}" ] ; then
-	errcho "Error: Directory '${internals}' not found."
+__tps_init__="${__scripts__}/internal/tps_init.sh"
+__tps_init_file__="${base_dir}/${__tps_init__}"
+
+if [ ! -f "${__tps_init_file__}" ] ; then
+	errcho "Error: File '${__tps_init__}' not found."
 	exit 2
 fi
 
-if [ ! -f "${tps_init_file}" ] ; then
-	errcho "Error: File '${tps_init}' not found."
-	exit 2
-fi
+source "${__tps_init_file__}"
 
-source "${tps_init_file}"
-
-if [ -f "${scripts_dir}/${command}.sh" ]; then
-	bash "${scripts_dir}/${command}.sh" "$@"
-elif [ -f "${scripts_dir}/${command}.py" ]; then
-	python "${scripts_dir}/${command}.py" "$@"
-elif [ -f "${scripts_dir}/${command}.exe" ]; then
-	"${scripts_dir}/${command}.exe" "$@"
+if [ -f "${__scripts_dir__}/${command}.sh" ]; then
+	bash "${__scripts_dir__}/${command}.sh" "$@"
+elif [ -f "${__scripts_dir__}/${command}.py" ]; then
+	python "${__scripts_dir__}/${command}.py" "$@"
+elif [ -f "${__scripts_dir__}/${command}.exe" ]; then
+	"${__scripts_dir__}/${command}.exe" "$@"
 else
-	errcho "Error: command '${command}' not found in '${scripts}'".
+	errcho "Error: command '${command}' not found in '${__scripts__}'".
 	errcho "Searched for '${command}.sh', '${command}.py', '${command}.exe'."
 	exit 2
 fi
