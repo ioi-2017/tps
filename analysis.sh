8f332f8fe12b8329784f9d4cad23c0fb8d26435f
Hamed Saleh <hameelas@gmail.com>
2017-06-30 21:13:01 +0430

    Add analysis.sh

---
+++ analysis.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+tps_url="https://tps.ioi2017.org/gitted/problem"
+
+commit=$(git log --pretty=format:'%H' -n 1)
+
+repo_url=$(git config --local remote.origin.url)
+
+if [ -z ${repo_url+x} ]; then
+    errcho "Cannot find the 'origin' remote url to extract repo name"
+    exit 1
+fi
+
+repo_name=$(basename -s .git ${repo_url})
+
+analysis_url=${tps_url}/${repo_name}/${commit}/analysis
+
+
+echo ${analysis_url}
+
+# python -mwebbrowser ${analysis_url}
+
+if which xdg-open; then
+    xdg-open ${analysis_url}
+elif which gnome-open; then
+    gnome-open ${analysis_url}
+elif which open; then
+    open ${analysis_url}
+else
+    start ${analysis_url}
+fi
