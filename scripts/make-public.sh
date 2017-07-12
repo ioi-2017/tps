68ab760455042774e954b02340c6c7b509eacd18
Hamed Saleh <hameelas@gmail.com>
2017-07-12 21:11:12 +0430

    Update make-public.sh

--- scripts/make-public.sh
+++ scripts/make-public.sh
@@ -10,23 +10,27 @@ public="${public_dir}"
 attachment_name="${problem_name}.zip"
 public_files="${templates}/public.files"
 
-function pgg() {
-	echo pgg: "$@"
-	python ${internals}/pgg.py < "${grader}/$1" > "${public}/$1"
-	fix "$@"
-}
-
 function fix() {
-	echo fix: "$@"
-	dos2unix "${public}/$1" &> /dev/null
+	cecho yellow -n "fix: "
+	echo "$@"
+	check_file_exists "file" "${public}/$1"
+	dos2unix -q "${public}/$1" > /dev/null
 }
 
-function make_public() {
+function pgg() {
+	cecho yellow -n "pgg: "
+	echo "$@"
+	check_file_exists "file" "${grader}/$1"
+	python ${internals}/pgg.py < "${grader}/$1" > "${public}/$1"
 	fix "$@"
 }
 
 function make_grader() {
 	pgg "$@"
+}
+
+function make_public() {
+	fix "$@"
 }
 
 extention_point="${templates}/make_public_extension.sh"
@@ -68,5 +72,9 @@ while read raw_line; do
 done < $public_files | zip -@ "${attachment_name}"
 
 popd > /dev/null
+
+mv "${public_dir}/${attachment_name}" .
+
+cecho yellow "Created attachment '${attachment_name}'."
 
 cecho green OK
