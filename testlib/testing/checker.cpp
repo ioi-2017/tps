d6b868c20c8b851821e065eac998e6058b2e7630
Kian Mirjalali <mirjalali@gmail.com>
2017-07-25 03:00:50 +0430

    added tests for testlib-ioi.h

---
+++ testlib/testing/checker.cpp
@@ -0,0 +1,14 @@
+#include "testlib.h"
+using namespace std;
+
+static string output_secret = "the_output_secret_key";
+
+int main(int argc, char * argv[])
+{
+	registerChecker("mountains", argc, argv);
+
+	readBothSecrets(output_secret);
+	readBothGraderResults();
+
+	compareRemainingLines(3);
+}
