c14e978dc2dc9975ff293b98688d1b4f0d84a825
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2021-02-28 23:40:34 +0800

    Show score and num of tests invoked per subtask

--- scripts/internal/invoke.py
+++ scripts/internal/invoke.py
@@ -2,13 +2,14 @@ import sys
 import os
 import subprocess
 
-from util import get_bool_environ, simple_usage_message, wait_process_success
-from color_util import cprinterr, colors
+from util import get_bool_environ, load_json, simple_usage_message, wait_process_success
+from color_util import cprint, cprinterr, colors
 import tests_util as tu
 
 
 INTERNALS_DIR = os.environ.get('INTERNALS')
 LOGS_DIR = os.environ.get('LOGS_DIR')
+SUBTASKS_JSON = os.environ.get('SUBTASKS_JSON')
 SPECIFIC_TESTS = get_bool_environ('SPECIFIC_TESTS')
 SPECIFIED_TESTS_PATTERN = os.environ.get('SPECIFIED_TESTS_PATTERN')
 
@@ -33,8 +34,6 @@ if __name__ == '__main__':
     if missing_tests:
         cprinterr(colors.WARN, "Missing tests: "+(", ".join(missing_tests)))
 
-    subtask_score_and_verdict = dict.fromkeys(tu.get_subtasks_tests_dict_from_tests_dir(tests_dir).keys())
-
     for test_name in available_tests:
         command = [
             'bash',
@@ -44,29 +43,60 @@ if __name__ == '__main__':
         ]
         wait_process_success(subprocess.Popen(command))
 
-        score = verdict = None
-        with open(os.path.join(LOGS_DIR, "{}.score".format(test_name)), 'r') as sf:
-            score = float(sf.readlines()[0].strip('\n'))
-        with open(os.path.join(LOGS_DIR, "{}.verdict".format(test_name)), 'r') as vf:
-            verdict = vf.readlines()[0].strip('\n')
-
-        for subtask in tu.get_test_subtasks_from_tests_dir(test_name, tests_dir):
-            if subtask_score_and_verdict[subtask] is None or score < subtask_score_and_verdict[subtask][0]:
-                subtask_score_and_verdict[subtask] = (score, verdict, test_name)
-
     print("\nSubtask summary")
-    for subtask in subtask_score_and_verdict.keys():
-        if subtask_score_and_verdict[subtask] is not None:
-            score, verdict, test_name = subtask_score_and_verdict[subtask]
+
+    subtasks_data = dict(load_json(SUBTASKS_JSON))['subtasks']
+    total_points = total_full_points = 0
+    for subtask, tests in tu.get_subtasks_tests_dict_from_tests_dir(tests_dir).items():
+        subtask_result = None
+        testcases_run = 0
+
+        for test in tests:
+            score = verdict = None
+            try:
+                with open(os.path.join(LOGS_DIR, "{}.score".format(test)), 'r') as sf:
+                    score = float(sf.readlines()[0].strip('\n'))
+                with open(os.path.join(LOGS_DIR, "{}.verdict".format(test)), 'r') as vf:
+                    verdict = vf.readlines()[0].strip('\n')
+            except FileNotFoundError:
+                pass
+            else:
+                if subtask_result is None or score < subtask_result[0]:
+                    subtask_result = (score, verdict, test)
+                testcases_run += 1
+
+        if subtask_result is None:
             command = [
                 'bash',
                 os.path.join(INTERNALS_DIR, 'subtask_summary.sh'),
                 subtask,
-                str(score),
-                verdict,
-                test_name
+                str(len(tests))
             ]
             wait_process_success(subprocess.Popen(command))
+        else:
+            subtask_score = subtask_result[0] * subtasks_data[subtask]['score']
+            command = [
+                'bash',
+                os.path.join(INTERNALS_DIR, 'subtask_summary.sh'),
+                subtask,
+                str(len(tests)),
+                str(testcases_run),
+                '{:g}'.format(round(subtask_score, 2)),
+                str(subtasks_data[subtask]['score']),
+                subtask_result[1],
+                subtask_result[2]
+            ]
+            wait_process_success(subprocess.Popen(command))
+
+            total_points += subtask_score
+            total_full_points += subtasks_data[subtask]['score']
+
+    color = colors.OK
+    if total_points == 0:
+        color = colors.ERROR
+    elif total_points < total_full_points:
+        color = colors.WARN
+    cprint(color, "{:g}/{} pts".format(round(total_points, 2), total_full_points))
 
     if missing_tests:
         cprinterr(colors.WARN, "Missing {} {}!".format(len(missing_tests), "tests" if len(missing_tests) != 1 else "test"))
--- scripts/internal/subtask_summary.sh
+++ scripts/internal/subtask_summary.sh
@@ -5,16 +5,39 @@ set -euo pipefail
 source "${INTERNALS}/util.sh"
 
 subtask_name="$1"; shift
-score="$1"; shift
-verdict="$1"; shift
-test_name="$1"; shift
+num_tests="$1"; shift
 
 printf "%-${STATUS_PAD}s" "${subtask_name}"
-printf "%5s" "${score}"
-hspace 2
-export BOX_PADDING=20
-echo_verdict "${verdict}"
+if [ $# -gt 0 ]; then
+  num_tests_run="$1"; shift
+  subtask_score="$1"; shift
+  full_subtask_score="$1"; shift
+  verdict="$1"; shift
+  test_name="$1"; shift
 
-printf "${test_name}"
+  if [ "${num_tests_run}" -eq "${num_tests}" ]; then
+    tests_color="ok"
+  else
+    tests_color="warn"
+  fi
+  export BOX_PADDING=11
+  boxed_echo "${tests_color}" "${num_tests_run}/${num_tests} tests"
+  hspace 2
+  export BOX_PADDING=20
+  echo_verdict "${verdict}"
 
-echo
+  export BOX_PADDING=11
+  if [ "${subtask_score}" == "0" ] && [ "${verdict}" != "Correct" ]; then
+    subtask_score_color="fail"
+  elif [ "${subtask_score}" == "${full_subtask_score}" ]; then
+    subtask_score_color="ok"
+  else
+    subtask_score_color="warn"
+  fi
+  boxed_echo "${subtask_score_color}" "${subtask_score}/${full_subtask_score} pts"
+  hspace 2
+  echo "${test_name}"
+else
+  boxed_echo "fail" "0/${num_tests} tests"
+  echo
+fi
