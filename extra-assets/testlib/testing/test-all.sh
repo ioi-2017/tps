e8e341c1b4c15b5d4d681cf3c7cc6e30c1fac9f2
Kian Mirjalali <mirjalali@gmail.com>
2021-06-08 12:46:37 +0430

    Add testlib to `extra-asssets`
    * testlib-0.9.12 (used in IOI2017)
    * Add the original & modified versions of testlib.h
    * Add the simple behavioral tests

---
+++ extra-assets/testlib/testing/test-all.sh
@@ -0,0 +1,13 @@
+
+if ! make ; then
+	exit;
+fi
+
+
+T="emp onosec gmis ginv gsv gwe gwr mis dif ext ext2 cor "
+for i in $T ; do
+	echo out $i:
+	./checker.exe 01.in 01.out $i.out
+	echo $?
+	echo =========
+done
