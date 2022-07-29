b10618f57047d52dbd7132cc677e5691a2a7dc62
Kian Mirjalali <mirjalali@gmail.com>
2022-07-29 07:20:06 +0430

    [Scripts] Refactor `gen_test.sh`: improve quality of code

--- scripts/internal/gen_test.sh
+++ scripts/internal/gen_test.sh
@@ -10,19 +10,29 @@ tests_dir="$1"; shift
 test_name="$1"; shift
 gen_command_line=("$@")
 
-input="${tests_dir}/${test_name}.in"
-output="${tests_dir}/${test_name}.out"
+input_file_name="${test_name}.in"
+output_file_name="${test_name}.out"
+
+input_file_path="${tests_dir}/${input_file_name}"
+output_file_path="${tests_dir}/${output_file_name}"
 
 
 final_ret=0
 failed_jobs=""
 
+function add_failed_job {
+	local -r job_name="$1"; shift
+	local -r ret="$1"; shift
+	final_ret="${ret}"
+	failed_jobs="${failed_jobs} ${job_name}"
+}
+
 function verify_job_failure {
-	local job="$1"
-	local ret=$(warning_aware_job_ret "${job}")
-	if [ ${ret} -ne 0 ]; then
-		final_ret=${ret}
-		failed_jobs="${failed_jobs} ${job}"
+	local -r job_name="$1"; shift
+	local ret
+	ret="$(warning_aware_job_ret "${job_name}")"
+	if [ "${ret}" -ne "0" ]; then
+		add_failed_job "${job_name}" "${ret}"
 	fi
 }
 
@@ -34,23 +44,23 @@ export BOX_PADDING=7
 echo -n "gen"
 gen_job="${test_name}.gen"
 if ! "${SKIP_GEN}"; then
-	insensitive guard "${gen_job}" gen_input "${input}" "${gen_command_line[@]}"
+	insensitive guard "${gen_job}" gen_input "${input_file_path}" "${gen_command_line[@]}"
 	verify_job_failure "${gen_job}"
 fi
-gen_status=$(job_status "${gen_job}")
+gen_status="$(job_status "${gen_job}")"
 echo_status "${gen_status}"
 
 
 function validate {
-	if [ ! -f "${input}" ]; then
-		errcho "input file ${test_name}.in is not available"
+	if [ ! -f "${input_file_path}" ]; then
+		errcho "input file ${input_file_name} is not available"
 		return 4
 	fi
 
 	get_test_validator_commands "${tests_dir}" "${test_name}" | while read validator_command; do
 		[ -z "${validator_command}" ] && continue
 		errcho "starting validator command: ${validator_command}"
-		eval "${validator_command}" < "${input}" || return $?
+		eval "${validator_command}" < "${input_file_path}" || return $?
 		errcho "OK"
 	done || return $?
 }
@@ -61,18 +71,18 @@ if ! "${SKIP_VAL}" && ! is_in "${gen_status}" "FAIL"; then
 	insensitive guard "${val_job}" validate
 	verify_job_failure "${val_job}"
 fi
-val_status=$(job_status "${val_job}")
+val_status="$(job_status "${val_job}")"
 echo_status "${val_status}"
 
 
 function gen_output {
-	if [ ! -f "${input}" ]; then
-		errcho "input file ${test_name}.in is not available"
+	if [ ! -f "${input_file_path}" ]; then
+		errcho "input file ${input_file_name} is not available"
 		return 4
 	fi
-	temp_output=${output}.tmp
-	bash "${SCRIPTS}/run.sh" < "${input}" > "${temp_output}" || return $?
-	mv "${temp_output}" "${output}"
+	local -r temp_output="${output_file_path}.tmp"
+	bash "${SCRIPTS}/run.sh" < "${input_file_path}" > "${temp_output}" || return $?
+	mv "${temp_output}" "${output_file_path}"
 }
 
 echo -n "sol"
@@ -81,21 +91,18 @@ if ! "${SKIP_SOL}" && ! is_in "${gen_status}" "FAIL"; then
 	insensitive guard "${sol_job}" gen_output
 	verify_job_failure "${sol_job}"
 fi
-sol_status=$(job_status "${sol_job}")
+sol_status="$(job_status "${sol_job}")"
 echo_status "${sol_status}"
 
 
 echo
 
 
-if "${SENSITIVE_RUN}"; then
-	if [ ${final_ret} -ne 0 ]; then
-		for job in ${failed_jobs}; do
-			echo
-			echo "failed job: ${job}"
-			execution_report "${job}"
-		done
-
-		exit ${final_ret}
-	fi
+if "${SENSITIVE_RUN}" && [ "${final_ret}" -ne "0" ]; then
+	for job in ${failed_jobs}; do
+		echo
+		echo "failed job: ${job}"
+		execution_report "${job}"
+	done
+	exit "${final_ret}"
 fi
